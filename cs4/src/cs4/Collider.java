761ae50a8c0643f6048c458938f988249b017d3c
Ajyoop <48433644+Ajyoop@users.noreply.github.com>
2021-04-21 11:20:06 +0200

    Created main classes
    The basic classes of the project were created.

---
+++ cs4/src/cs4/Collider.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4;
+
+/**
+ *
+ * @author elliot.vesterlund
+ */
+public class Collider {
+    private double xCoord;
+    private double yCoord;
+    private int height;
+    private int width;
+
+    public Collider(double xCoord, double yCoord, int height, int width) {
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.height = height;
+        this.width = width;
+    }
+    
+    
+    public void move(){
+        
+    }
+    
+        
+    
+}
